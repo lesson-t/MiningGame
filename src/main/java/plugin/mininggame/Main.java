10210f3f4dcf463abcecb05b0aa4b15332176a20
takeshi <goozooloo123@gmail.com>
2024-03-15 09:49:56 +0900

    refactoring5

--- src/main/java/plugin/mininggame/Main.java
+++ src/main/java/plugin/mininggame/Main.java
@@ -1,11 +1,12 @@
 package plugin.mininggame;
 
+import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.mininggame.command.MaterialSpawnCommand;
 import plugin.mininggame.command.MiningGameCommand;
 
-public final class Main extends JavaPlugin {
+public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -13,8 +14,8 @@ public final class Main extends JavaPlugin {
         MaterialSpawnCommand materialSpawnCommand = new MaterialSpawnCommand();
         Bukkit.getPluginManager().registerEvents(miningGameCommand, this);
         Bukkit.getPluginManager().registerEvents(materialSpawnCommand, this);
-        getCommand("mininggame").setExecutor(miningGameCommand);
-        getCommand("materialSpawn").setExecutor(materialSpawnCommand);
+        Objects.requireNonNull(getCommand("mininggame")).setExecutor(miningGameCommand);
+        Objects.requireNonNull(getCommand("materialSpawn")).setExecutor(materialSpawnCommand);
 
     }
 
--- src/main/java/plugin/mininggame/command/MiningGameCommand.java
+++ src/main/java/plugin/mininggame/command/MiningGameCommand.java
@@ -2,15 +2,10 @@ package plugin.mininggame.command;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
